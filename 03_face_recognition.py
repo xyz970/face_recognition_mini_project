07408486ea08615d3c9eba676c07f071a308bae9
mhmmdadisptro <muhammadxxz7@gmail.com>
2023-10-06 23:55:09 +0700

    Update 03_face_recognition.py
    memperbaiki fungsi untuk mengecek confidence
    sekarang hanya yang diatas 40% akan diterima dan servo akan bergerak

--- 03_face_recognition.py
+++ 03_face_recognition.py
@@ -19,65 +19,60 @@ board.digital[5].write(1)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
+# iniciate id counter
 id = 0
-
 
 names = ['None', 'Adi']
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
 
 # Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
 
 while True:
 
-    ret, img =cam.read()
+    ret, img = cam.read()
     # img = cv2.flip(img, -1) # Flip vertically
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale( 
+    faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
+    for (x, y, w, h) in faces:
 
-    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-
-        # Check if confidence is less them 100 ==> "0" is perfect match 
-        if confidence < 100:
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 100) or (confidence >= 40):
             id = names[id]
-            count = 0
             confidence = "  {0}%".format(round(100 - confidence))
-            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
-            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
-            cv2.imshow('camera', img)
-
-            # pw = str(input("Masukkan password"))
-            # if pw == '12345':
-            #     getServo.write(25)
-            #     count = 1
-            # else:
-            #     getServo.write(0)
             getServo.write(25)
-        elif confidence >= 30:
+        else:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
             getServo.write(0)
+        # print(getServo.read())
+        if getServo.read() == 25:
+            print(f'Halo {id}')
+            print("Pintu terbuka.. Pintu akan otomatis menutup dalam 5 detik")
+            time.sleep(5)
+            getServo.write(0)
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
+    cv2.imshow('camera', img)
     
-
-    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
 
